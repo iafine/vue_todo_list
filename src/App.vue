c28d3be6828262eef373f90eab73337f846a0f09
iafine <iafineden@gmail.com>
2019-02-26 14:47:49 +0800

    add todo list

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div>
+      <input v-model="message">
+      <button v-on:click="submitMessage">提交</button>
+    </div>
+    <ul>
+      <todo-item v-for="(item, index) in list" :key="index" :content="item" :index="index" v-on:delete="deleteItem"></todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+  data() {
+    return {
+      message: '',
+      list: []
+    }
+  },
+  components: {
+    TodoItem
+  },
+  methods: {
+    submitMessage: function() {
+      this.list.push(this.message)
+      this.message = ''
+    },
+    deleteItem: function(index, content) {
+      if (confirm('是否删除' + content + '?')) {
+        this.list.splice(index, 1)
+      }
+    }
+  }
+}
+
+</script>
+
+<style>
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <li v-on:click="clickItem"> {{ content }} </li>
+</template>
+
+<script>
+export default {
+    props: ['content', 'index'],
+    methods: {
+        clickItem: function() {
+            this.$emit('delete', this.index, this.content)
+        }
+    },
+}
+</script>
